44571025cb7c59a6dffa0b66649e558aa032a155
khw11044 <khw11044@naver.com>
2024-12-15 16:51:32 +0900

    update

--- utils.py
+++ utils.py
@@ -93,53 +93,62 @@ h36m_coco_order = [9, 11, 14, 12, 15, 13, 16, 4, 1, 5, 2, 6, 3]
 coco_order = [0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 spple_keypoints = [10, 8, 0, 7]
 
-import numpy as np
-
-h36m_coco_order = [9, 11, 14, 12, 15, 13, 16, 4, 1, 5, 2, 6, 3]
-coco_order = [0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-spple_keypoints = [10, 8, 0, 7]
 
 def coco_h36m(keypoints):
     temporal = keypoints.shape[0]
     keypoints_h36m = np.zeros_like(keypoints, dtype=np.float32)
     htps_keypoints = np.zeros((temporal, 4, 2), dtype=np.float32)
+    
+    # 코 0 
+    # tmp_nose_points = keypoints[:, [3,4], :]
+    valid_nose_points = np.array([k for k in keypoints[:, [3, 4], :][0] if np.all(k != 0)])  # 모든 값이 0이 아닌 행만 포함
+    tmp_nose_points = np.mean(valid_nose_points, axis=0) if valid_nose_points.size>0 else np.array([0.0, 0.0])  
+    nose_points = keypoints[:, 0, :]
+    keypoints[:, 0, :] = tmp_nose_points if np.any(nose_points == 0) else nose_points  
 
     # htps_keypoints: head(0:10), thorax(1:8), pelvis(2:0), spine(3:7)
-    # Head (10의 x 값)
-    valid_points = [k for k in keypoints[:, 1:5, 0][0] if k != 0]
-    htps_keypoints[:, 0, 0] = np.mean(valid_points) if valid_points else 0.0
+    # Thorax (8) 목 x
+    thorax_points_x = [k for k in keypoints[:, [5,6], 0][0] if k != 0]
+    htps_keypoints[:, 1, 0] = np.mean(thorax_points_x) if thorax_points_x else np.array([0.0])
+    # Thorax (8) 목 y
+    thorax_points_y = [k for k in keypoints[:, [5,6], 1][0] if k != 0]
+    htps_keypoints[:, 1, 1] = np.mean(thorax_points_y) if thorax_points_y else np.array([0.0]) 
+    if not np.all(htps_keypoints[:, 1, :] == np.array([[0.0,0.0]])) and not np.all(keypoints[:, 0, :] == np.array([[0.0,0.0]])):
+        htps_keypoints[:, 1, :] += (keypoints[:, 0, :] - htps_keypoints[:, 1, :]) / 3
+    
+    
+    # Head (10) 머리 x
+    head_points_x = [k for k in keypoints[:, 1:5, 0][0] if k != 0]
+    htps_keypoints[:, 0, 0] = np.mean(head_points_x) if head_points_x else np.array([0.0])
 
-    # Head (10의 y 값)
-    valid_points_y = [k for k in keypoints[:, 1:3, 1][0] if k != 0]
-    mean_y = np.mean(valid_points_y) if valid_points_y else keypoints[:, 0, 1]
-    htps_keypoints[:, 0, 1] = keypoints[:, 0, 1] - 4 * abs(mean_y - keypoints[:, 0, 1])
+    # Head (10) 머리 y
+    head_points_y = [k for k in keypoints[:, [1,2], 1][0] if k != 0]
+    htps_keypoints[:, 0, 1] = keypoints[:, 0, 1] - 4 * abs(np.mean(head_points_y) - keypoints[:, 0, 1]) if head_points_y else keypoints[:, 0, 1] - abs(htps_keypoints[:, 1, 1] - keypoints[:, 0, 1])
+    
+    htps_keypoints[:, 0, :] = np.array([[0.0,0.0]]) if np.any(htps_keypoints[:, 0, :] == 0) else htps_keypoints[:, 0, :] 
 
-    # Thorax (8)
-    thorax_points = keypoints[:, 5:7, :][~np.all(keypoints[:, 5:7, :] == 0, axis=2)]
-    htps_keypoints[:, 1, :] = np.mean(thorax_points, axis=0) if thorax_points.size else 0.0
-    htps_keypoints[:, 1, :] += (keypoints[:, 0, :] - htps_keypoints[:, 1, :]) / 3
-
-    # Pelvis (0)
+    # Pelvis (0) 골반 중앙 
     pelvis_points = keypoints[:, 11:13, :]
-    htps_keypoints[:, 2, :] = (
-        np.mean(pelvis_points, axis=1) if np.any(pelvis_points != 0) else 0.0
-    )
+    htps_keypoints[:, 2, :] = np.array([[0.0,0.0]]) if np.any(pelvis_points == 0) else np.mean(pelvis_points, axis=1)  
 
-    # Spine (7)
+    # Spine (7) 명치
     spine_points = keypoints[:, [5, 6, 11, 12], :]
-    htps_keypoints[:, 3, :] = (
-        np.mean(spine_points, axis=1) if np.any(spine_points != 0) else 0.0
-    )
+    htps_keypoints[:, 3, :] = np.array([[0.0,0.0]]) if np.any(spine_points == 0) else np.mean(spine_points, axis=1)  
 
     # 최종 h36m 포맷에 맞게 keypoints 설정
     keypoints_h36m[:, spple_keypoints, :] = htps_keypoints
     keypoints_h36m[:, h36m_coco_order, :] = keypoints[:, coco_order, :]
 
-    # Spine 조정
-    keypoints_h36m[:, 8, 1] -= (np.mean(keypoints[:, 1:3, 1], axis=1, dtype=np.float32) - keypoints[:, 0, 1]) * 2 / 3
+    # Thorax 조정
+    # keypoints_h36m[:, 8, 1] -= (np.mean(keypoints[:, 1:3, 1], axis=1, dtype=np.float32) - keypoints[:, 0, 1]) * 2 / 3
 
     # 유효한 프레임 찾기
     valid_frames = np.where(np.sum(keypoints_h36m.reshape(-1, 34), axis=1) != 0)[0]
+
+    # NaN 값이 있는지 확인 후 0으로 치환
+    if np.isnan(keypoints_h36m).any():
+        print("NaN 값이 발견되었습니다. 0으로 치환합니다.")
+        np.nan_to_num(keypoints_h36m, copy=False, nan=0.0)
     
     return keypoints_h36m, valid_frames
 
@@ -169,3 +178,11 @@ def h36m_coco_format(keypoints, scores):
     h36m_scores = np.asarray(h36m_scores, dtype=np.float32).reshape(-1,17,1)
 
     return h36m_kpts, h36m_scores, valid_frames
+
+
+# normalize_screen_coordinates 
+def normalize2dhp(X, w, h): 
+    assert X.shape[-1] == 2
+    
+    # Normalize so that [0, w] is mapped to [-1, 1], while preserving the aspect ratio
+    return X/w*2 - [1, h/w]
